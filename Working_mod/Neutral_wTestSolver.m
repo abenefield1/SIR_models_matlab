1cfd9cecb36d3ba2f90b0469518605e8499d9c4c
Amy Benefield <amy.benefield@colorado.edu>
2019-06-03 12:54:14 -0600

    for loop for 'd' nested inside main function

--- Working_mod/Neutral_wTestSolver.m
+++ Working_mod/Neutral_wTestSolver.m
@@ -5,9 +5,11 @@ S0 = 1000; % initial susceptibles
 I0 = 1; % initial base strain/class infection
 n = 60; % number of classes possible;
 totalTime = 120;
-a = 1; % immunity distance parameter
+a = 0.9; % immunity distance parameter
 b = 100;
-d = 0.8;
+%d = 0.8;
+%dMax=.98;
+
 
 y0 = [S0; I0; zeros(n,1)]; % initial conditions as column vector
 myBeta = 1.2;  % transmission parameter
@@ -16,7 +18,7 @@ nu = 0.2;   %
 
 % dydt = NeutralModelFn(t, y, myBeta, nu, mu)
 
-[time, abundances] = ode45( @(time, abundances) Neutral_wTest(time, abundances, myBeta, nu, mu, b, d, a), [0, totalTime], y0 );
+[time, abundances] = ode45( @(time, abundances) Neutral_wTest(time, abundances, myBeta, nu, mu, b, a), [0, totalTime], y0 );
 
 
 % cols=[3:5:n];
@@ -31,3 +33,12 @@ for i = 1:length(plotCols)
     plot(time, abundances(:,plotCols(i)));
 end
 
+
+
+% function detection = d(k, a)
+%     sensitivity = 0.78; % test sensitivity - from Harkins and Munson:low end of Commercial Nucleic Acid Hybridization test on 16s rRNA
+%     testRate = 0.0483; % https://www.cdc.gov/std/stats17/chlamydia.htm
+%     a = 0.12; % testing escape parameter - the higher a, the more rapid the escape
+%     dMax = sensitivity * testRate;
+%     detection = dMax * exp(-k * a);
+% end
