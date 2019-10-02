d83772ffe3bee38d4fe9732f832eabf7e3ac5bc4
Amy Benefield <amy.benefield@colorado.edu>
2019-10-02 14:48:26 -0600

    varied detection rate

--- Simplified_model/simpModDetSolve.m
+++ Simplified_model/simpModDetSolve.m
@@ -25,7 +25,7 @@ R=class(:,3);
 
 %% 100:150 - post vaccination
 
-DetVec = [0, 1, 0.5, 0.0001];
+DetVec = [0, 0.0001, 0.09, 0.11, 0.5, 1];
 n = length(DetVec);
 figure
 for i = 1:n
@@ -36,8 +36,30 @@ for i = 1:n
     S=class2(:,1);
     I=class2(:,2);
     R=class2(:,3);
+    
+    
+    subplot(0.5*n,2,i)
+    plot(t,S,'g','LineWidth',2); hold on
+    plot(t,I,'r','LineWidth',2); hold on
+    plot(t,R,'b','LineWidth',2); hold on
+    %axis([0 50 0 500])
+    ylabel('Incidence')
+    h=legend('Susceptible (S)', 'Infected (I)','Recovered (R)','Location','northwest');
+end
+xlabel('Years')
 
-
+%%
+DetVec = [0, 0.09, 0.11, 1];
+n = length(DetVec);
+figure
+for i = 1:n
+    det = DetVec(i);
+    sigma=0.5;
+    deltaI=0.2;
+    [t, class2]=ode45(@(t, class) simpModDet(t, class, N, beta, nu, b, D, det), T2, class(size(class,1),:) );
+    S=class2(:,1);
+    I=class2(:,2);
+    R=class2(:,3);
     
     
     subplot(n,1,i)
@@ -46,7 +68,6 @@ for i = 1:n
     plot(t,R,'b','LineWidth',2); hold on
     %axis([0 50 0 500])
     ylabel('Incidence')
-   % h=legend('R',-1);
     h=legend('Susceptible (S)', 'Infected (I)','Recovered (R)','Location','northwest');
 end
 xlabel('Years')
